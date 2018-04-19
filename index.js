cab6af22450c560f7496eed6605da9ba87f2ca59
Matt Zuzolo <mattzuzolo@outlook.com>
2018-04-19 00:40:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ var recipes = {
 };
 
 
-function updateObjectWithKeyAndValue (obj, key, value) {
+// function updateObjectWithKeyAndValue (obj, key, value) {
   
   
-  return Object.assign( {}, { [key]: value}   );
+//   return Object.assign( {}, { [key]: value}   );
   
+// }
+
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
 }
-
-
