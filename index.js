c6d709e9a1f44c0f12cd4484e698438ab7fe13b4
Matt Zuzolo <mattzuzolo@outlook.com>
2018-04-19 00:35:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ var recipes = {
 
 function updateObjectWithKeyAndValue (obj, key, value) {
   
-  // obj.key = value;
-  
-  // // //returns an object with original key value pairs and the new key value pair
-  // return;
+
 }
 
 
